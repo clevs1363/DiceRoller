9859bdf984a8bdd6ea81c3c3f7d027b43d7cba94
Michael Cleversley <cleversleym13@gmail.com>
2021-10-04 09:42:42 -0400

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>Dice Roller</h1>
+    </header>    
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped> /* scoped means only this component receives this styling */
+header {
+    color: green;
+}
+</style>
